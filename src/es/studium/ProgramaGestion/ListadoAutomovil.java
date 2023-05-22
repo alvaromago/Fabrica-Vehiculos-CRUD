c58bdd0e18f2d0c3f4507c0f346dadefdd9a3adb
alvaromago <aalvaromago@gmail.com>
2023-05-22 13:51:12 +0200

    Imports

--- src/es/studium/ProgramaGestion/ListadoAutomovil.java
+++ src/es/studium/ProgramaGestion/ListadoAutomovil.java
@@ -17,6 +17,7 @@ import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
